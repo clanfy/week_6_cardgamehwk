e9fd263b9fc29619f7c3c1968382a93b16f18f09
Clacson <cookiellacson@gmail.com>
2016-11-06 15:02:55 +0000

    player created and tests passed

---
+++ game/Player.java
@@ -0,0 +1,16 @@
+package game;
+import game.*;
+
+public class Player {
+
+  private String name;
+
+  public Player (String name){
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+}
